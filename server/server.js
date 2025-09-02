42fce5d9f4e381c141b6d06685cf8b839872919a
mr-fahad-03 <mdfahadzulfiqar@gmail.com>
2025-09-02 12:00:57 +0500

    Server fixed on 3000

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Blog API Server Running" })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT =3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
